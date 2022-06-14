3ee4f486a5cf8417b545dd0ca8a8cb4196084635
Brian Demers <bdemers@apache.org>
2022-06-14 18:20:00 -0400

    Update web-app to use Okta LDAP Interface

--- src/main/java/com/okta/example/ra/Application.java
+++ src/main/java/com/okta/example/ra/Application.java
@@ -1,33 +1,22 @@
 package com.okta.example.ra;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.ldap.core.support.BaseLdapPathContextSource;
-import org.springframework.ldap.core.support.LdapContextSource;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.provisioning.JdbcUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.ldap.EmbeddedLdapServerContextSourceFactoryBean;
 import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 import org.springframework.security.ldap.userdetails.DefaultLdapAuthoritiesPopulator;
 import org.springframework.security.ldap.userdetails.PersonContextMapper;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.sql.DataSource;
-import java.util.Collections;
 import java.util.Map;
 
 @SpringBootApplication
@@ -48,15 +37,21 @@ public class Application {
     }
 
     @Bean
-    AuthenticationManager ldapAuthenticationManager(BaseLdapPathContextSource contextSource) {
+    AuthenticationManager ldapAuthenticationManager(BaseLdapPathContextSource contextSource, @Value("${okta.ldap.id}")String oktaLdapId) {
         LdapBindAuthenticationManagerFactory factory = new LdapBindAuthenticationManagerFactory(contextSource);
-        factory.setUserDnPatterns("cn={0},ou=users");//,dc=example,dc=org
+        factory.setUserDnPatterns("uid={0}@example.com,ou=users", "uid={0},ou=users"); // allow users to sign in with username or email address
         factory.setUserDetailsContextMapper(new PersonContextMapper());
 
-        DefaultLdapAuthoritiesPopulator ldapAuthoritiesPopulator = new DefaultLdapAuthoritiesPopulator(contextSource, "ou=groups");
-        ldapAuthoritiesPopulator.setGroupSearchFilter("(memberUid={0})");
-        factory.setLdapAuthoritiesPopulator(ldapAuthoritiesPopulator);
+        // if you want all Okta groups (Okta defined and original LDAP Groups)
+        // DefaultLdapAuthoritiesPopulator ldapAuthoritiesPopulator = new DefaultLdapAuthoritiesPopulator(contextSource, "ou=groups");
+        // ldapAuthoritiesPopulator.setGroupSearchFilter("(uniqueMember={0})");
+        // ldapAuthoritiesPopulator.setSearchSubtree(true);
 
+        // otherwise if you just want the original LDAP Groups
+        DefaultLdapAuthoritiesPopulator ldapAuthoritiesPopulator = new DefaultLdapAuthoritiesPopulator(contextSource, "cn=" + oktaLdapId + ",ou=apps,ou=groups");
+        ldapAuthoritiesPopulator.setGroupSearchFilter("(uniqueMember={0})");
+
+        factory.setLdapAuthoritiesPopulator(ldapAuthoritiesPopulator);
         return factory.createAuthenticationManager();
     }
 
